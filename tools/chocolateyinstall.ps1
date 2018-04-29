771dd9b739e716296775612ca276fbdb88c775ec
Bob Tanner <tanner@real-time.com>
2018-04-29 17:46:58 -0500

    First attempt at getting SYSPRO 7 Client to install

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,81 +1,31 @@
 ﻿$ErrorActionPreference = 'Stop'; 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
-$fileLocation = '\\readynas-1.int.celadonsystems.com\gold-images\SYSPRO\SYSPRO-v6.1-sp1\SYSPRO\SYSPRO 6.1 32-bit.exe'
 
-# https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#
+$pkg           = 'SYSPRO7Client.exe'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file          = Join-Path $toolsDir $pkg
+$checksum      = 'b5c0783f4a9a7ee63c1d48fa7e2e879a553f80496304cacd6b3d4a93f8c59b24'
+
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'exe'
-  file         = $fileLocation
-  softwareName  = 'SYSPRO*'
-  checksum      = 'CEA739BBEF18E18610A323FA6048E162019F213A3DD1EC02516976EC675B5CFC'
-  checksumType  = 'sha256' 
-  
-  # MSI
-  #silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  # OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
-  #validExitCodes= @(0) #please insert other valid exit codes here
+  packageName    = $env:ChocolateyPackageName
+  file           = $file
+  unzipLocation  = $toolsDir
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
-# https://chocolatey.org/docs/helpers-install-chocolatey-package
+# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 #
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs 
 
-## Main helper functions - these have error handling tucked into them already
-## see https://chocolatey.org/docs/helpers-reference
+# https://forum.fortinet.com/tm.aspx?m=150054
 
-## Install an application, will assert administrative rights
-## - https://chocolatey.org/docs/helpers-install-chocolatey-package
-## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-## add additional optional arguments as necessary
-##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+$installArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  file           = Join-Path $toolsDir 'FortiClient.msi'
+  fileType       = 'msi'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  validExitCodes = @(0, 3010, 1641)
+}
 
-## Download and unpack a zip file - https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-##Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
-
-## downloader that the main helpers use to download items
-## if removing $url64, please remove from here
-## - https://chocolatey.org/docs/helpers-get-chocolatey-web-file
-#Get-ChocolateyWebFile $packageName 'DOWNLOAD_TO_FILE_FULL_PATH' $url $url64
-
-## Unzips a file to the specified location - auto overwrites existing content
-## - https://chocolatey.org/docs/helpers-get-chocolatey-unzip
-#Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" $toolsDir
-    
-## Add specific files as shortcuts to the desktop
-## - https://chocolatey.org/docs/helpers-install-chocolatey-shortcut
-#$target = Join-Path $toolsDir "$($packageName).exe"
-# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
-
-## Outputs the bitness of the OS (either "32" or "64")
-## - https://chocolatey.org/docs/helpers-get-o-s-architecture-width
-#$osBitness = Get-ProcessorBits
-
-## Set persistent Environment variables
-## - https://chocolatey.org/docs/helpers-install-chocolatey-environment-variable
-#Install-ChocolateyEnvironmentVariable -variableName "SOMEVAR" -variableValue "value" [-variableType = 'Machine' #Defaults to 'User']
-
-## Set up a file association
-## - https://chocolatey.org/docs/helpers-install-chocolatey-file-association
-#Install-ChocolateyFileAssociation 
-
-##PORTABLE EXAMPLE
-#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
-#Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
-## END PORTABLE EXAMPLE
+Install-ChocolateyInstallPackage @installArgs
+Remove-Item -Force $packageArgs.file
