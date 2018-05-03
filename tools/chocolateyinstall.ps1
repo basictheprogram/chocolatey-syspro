79f73ae03671e1c7498b745f001e71be711ef038
Bob Tanner <tanner@real-time.com>
2018-05-03 18:10:37 -0500

    First attempt at a silent install using .msi file

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,31 +1,27 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
-$pkg           = 'SYSPRO7Client.exe'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file          = Join-Path $toolsDir $pkg
-$checksum      = 'b5c0783f4a9a7ee63c1d48fa7e2e879a553f80496304cacd6b3d4a93f8c59b24'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation   = Join-Path $toolsDir 'SYSPRO7Client.exe'
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  file           = $file
-  unzipLocation  = $toolsDir
-  checksum       = $checksum
-  checksumType   = 'sha256' 
-  softwareName   = 'SYSPRO 7 Client'
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' 
+  file          = $fileLocation
+  softwareName  = 'syspro-client*'
 
-}
+  # Checksums are now required as of 0.10.0.
+  #
+  checksum      = 'c6ddfaa90b25e5c404c3106c94f5fd49d8518ef561df591e1087d70d7d9a14ae'
+  checksumType  = 'sha256'
 
-# https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
-#
-Install-ChocolateyZipPackage @packageArgs 
-
-$installArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  file           = Join-Path $toolsDir $pkg
-  fileType       = 'msi'
-  silentArgs     = "/S /v/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
+  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
+  #
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
   validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @installArgs
-Remove-Item -Force $packageArgs.file
+# https://chocolatey.org/docs/helpers-install-chocolatey-install-package
+#
+Install-ChocolateyInstallPackage @packageArgs 
