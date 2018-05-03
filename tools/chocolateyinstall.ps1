a02c377d2c5c677392d5b061f1e9ad1698ee4b5e
Bob Tanner <tanner@real-time.com>
2018-05-03 18:41:39 -0500

    Add TRANSFORMS to silent installation
    Cleaned up .nuspec file

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation   = Join-Path $toolsDir 'SYSPRO7Client.exe'
+$fileLocation   = Join-Path $toolsDir 'SYSPRO7Client.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -18,7 +18,7 @@ $packageArgs = @{
   # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
   # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
   #
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`" TRANSFORMS=SYSPRO7_Runtime.mst"
   validExitCodes = @(0, 3010, 1641)
 }
 
