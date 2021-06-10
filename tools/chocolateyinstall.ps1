da62b3d3d0c9f021ce32f2ea70dc080578a2e64f
Bob Tanner <tanner@real-time.com>
2021-06-10 16:44:10 -0500

    WIP: #8
      * Moved private data into data.ps1
      * Ignore data.ps1 so it does not get committed to git

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$mstFile        = Join-Path $toolsDir 'SYSPRO7Client.mst'
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$mstFile = Join-Path $toolsDir $data.mstFile
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  silentArgs    = '/S /v"/qn /norestart TRANSFORMS="' + $mstFile + '"'
-  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/SYSPRO7Client.exe'
-  softwareName  = 'syspro-client'
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    fileType      = 'exe'
+    silentArgs    = '/S /v"/qn /norestart TRANSFORMS="' + $mstFile + '"'
+    softwareName  = 'syspro-client'
 
-  checksum      = 'b5c0783f4a9a7ee63c1d48fa7e2e879a553f80496304cacd6b3d4a93f8c59b24'
-  checksumType  = 'sha256'
+    url           = $data.url
+    checksum      = $data.checksum
+    checksumType  = $data.checksumType
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
